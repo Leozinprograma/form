73957a93f63182fa815a2d6a14efaec293fb8f99
Leonardo Meloto de Souza <143069584+Leozinprograma@users.noreply.github.com>
2024-05-30 20:32:26 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+//variaveis
+//Funcionais
+//condicionais
+//
+//funcoes parametrizada: generica, toda funcao que é criada,
+//ela precisa ser chamada
+// arrays:conjunto de informacoes
+
+
+let botao = document.querySelector("#botaoEnviar");
+
+
+
+
+function checkInputs() {
+    let inputs = document.querySelectorAll(".form-control input");
+    inputs.forEach(input => {
+        if (input.value == "") {
+        
+        }
+    })
+
+}
+
+
+function setError(input, message) {
+
+}
+
+function setSucess() {
+
+}
+
+
